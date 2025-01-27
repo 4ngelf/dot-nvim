b1a02ede69ae6ae071a5f62abd7065a8c3154bfe
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2025-01-27 00:23:13 -0400

    group plugin configurations with rocks api

--- lua/c11n/plugins/init.lua
+++
@@ -1,4 +0,0 @@
----@class c11n.plugins
---- Configuration scripts for plugins. See 'rocks.toml' for a complete list.
-local M = M(...)
-return M
---
+++ lua/c11n/rocks/config/init.lua
@@ -0,0 +1,3 @@
+---@class c11n.plugins
+--- Configuration scripts for plugins. As defined by 'rocks-config.nvim'
+--- See 'rocks.toml' for a complete list of plugins.
