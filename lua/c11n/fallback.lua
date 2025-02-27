a7771e5293429d1682c5a209790ccb53fb90a79d
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2025-02-27 10:25:32 -0400

    chore: edit fallback log

--- lua/c11n/fallback.lua
+++ lua/c11n/fallback.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 function M.init()
-  require("c11n.util").log "Using fallback implementation. You can install lazyvim with :C11n install_lazy"
+  require("c11n.util").log "using fallback implementation"
 
   for _, plugin in ipairs(require("c11n.settings").disabled_plugins) do
     vim.g["loaded_"..plugin] = true
