3dcd285bcd79f03b4a6763ac821218f9dc843168
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2025-03-15 19:24:38 -0400

    chore: improve description

--- lua/c11n/externals/windows.lua
+++ lua/c11n/externals/windows.lua
@@ -3,8 +3,8 @@ if not require("c11n").has("windows") then
   return
 end
 
--- HACK: On windows, uv.spawn only tries `.COM` and `.EXE` extensions. So, we will retry with other
--- extensions if those are not found. `on_exit` is only called when a running spawned process exits.
+-- HACK: On windows, uv.spawn only tries to find executables with `.COM` and `.EXE` extensions.
+-- When uv.spawn fails to find an executable, we will retry with other extensions.
 -- see: https://github.com/libuv/libuv/blob/4681d5d5705be932f82e1e79eff72f17b5bf82e2/src/win/process.c#L332
 do
   local uv_spawn = vim.uv.spawn
