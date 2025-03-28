7a595d306f5cae73b53d4e5e856ebdaedd19943c
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2025-03-28 12:26:36 -0400

    feat: move initialization logic to init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,16 @@
--- Neovim configuration entry point.
+--
+-- Neovim configuration
+--
+
 local nvim_version = "0.10.0"
 if vim.fn.has("nvim-" .. nvim_version) ~= 1 then
   vim.notify("neovim version required: >=" .. nvim_version, vim.log.levels.ERROR)
   return
 end
+
+--
+-- Get lazy.nvim
+--
 
 ---@type string
 local LAZY_PATH = vim.fs.joinpath(vim.fn.stdpath("data") --[[@as string]], "lazy", "lazy.nvim")
@@ -18,7 +25,6 @@ if not vim.uv.fs_stat(LAZY_PATH) then
       "https://github.com/folke/lazy.nvim.git",
       LAZY_PATH,
     }, {
-      text = true,
       stdout = false,
       stderr = false,
     })
@@ -37,5 +43,101 @@ if not pcall(require, "lazy") then
   return
 end
 
--- Initialize configuration
-require("c11n").init()
+local c11n = require("c11n")
+
+--
+-- Load configuration for external tools
+--
+
+require("lazy.core.util").lsmod("c11n.externals", function(modname, _)
+  require(modname)
+end)
+
+--
+-- Load management utilities
+--
+
+c11n.on_user("VeryLazy", function(_)
+  require("c11n.manage").setup()
+end)
+
+--
+-- Apply settings
+--
+
+local get_local_settings = loadfile(c11n.local_config_path)
+if get_local_settings then
+  local local_settings = get_local_settings()
+
+  local ok, error = pcall(c11n.settings.extend, local_settings)
+  if not ok then
+    c11n.on_user("VeryLazy", function(_)
+      vim.notify(error, vim.log.levels.ERROR)
+    end)
+  end
+end
+
+local settings = c11n.settings.get()
+
+-- Language
+local _ = pcall(vim.cmd.language, settings.language)
+
+---@type LazySpec
+local lazy_spec = {
+  -- add LazyVim and import its plugins
+  {
+    "LazyVim/LazyVim",
+    import = "lazyvim.plugins",
+    opts = {
+      colorscheme = settings.colorscheme,
+      news = {
+        lazyvim = true,
+        neovim = true,
+      },
+    },
+  },
+  -- default basic configuration
+  { import = "plugins._base" },
+}
+
+-- add selected preset
+if settings.preset then
+  ---@diagnostic disable-next-line: param-type-mismatch
+  table.insert(lazy_spec, { import = "plugins." .. settings.preset })
+end
+
+-- add selected preset
+if settings.lazyspec then
+  ---@diagnostic disable-next-line: param-type-mismatch
+  vim.list_extend(lazy_spec, settings.lazyspec)
+end
+
+---@type LazyConfig
+local lazy_config = {
+  spec = lazy_spec,
+  defaults = {
+    lazy = false,
+    version = false, -- always use the latest git commit
+  },
+  install = {
+    colorscheme = { settings.colorscheme, "habamax" },
+  },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = true, -- notify on update
+  },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = settings.disabled_plugins,
+    },
+  },
+}
+
+--
+-- Setup
+--
+
+require("lazy").setup(lazy_config)
+
+-- Initialization end
--- lua/c11n/init.lua
+++ lua/c11n/init.lua
@@ -1,113 +1,13 @@
 --- Initialization and utilities for configuration (c11n)
 local M = {}
 
-local util = require("c11n.util")
-local did_init = false
+M.util = require("c11n.util")
+M.has = M.util.has
+M.on_user = M.util.on_user
 
-M.has = util.has
+M.settings = require("c11n.settings")
 
 ---@type string
 M.local_config_path = vim.fs.joinpath(vim.fn.stdpath("data") --[[@as string]], "neovim.local.lua")
-
---- Load local machine configuration.
-local function load_local_config()
-  local extend_settings = require("c11n.settings").extend
-  local get_local_settings = loadfile(M.local_config_path)
-
-  if get_local_settings then
-    local local_settings = get_local_settings()
-    local ok, error = pcall(extend_settings, local_settings)
-    if not ok then
-      util.on_user("VeryLazy", function(_)
-        vim.notify(error, vim.log.levels.ERROR)
-      end)
-    end
-  end
-end
-
---- setup lazy.nvim
-local function lazy_setup()
-  local settings = require("c11n.settings").get()
-
-  ---@type LazySpec
-  local lazy_spec = {
-    -- add LazyVim and import its plugins
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {
-        colorscheme = settings.colorscheme,
-        news = {
-          lazyvim = true,
-          neovim = true,
-        },
-      },
-    },
-    -- default basic configuration
-    { import = "plugins._base" },
-  }
-
-  -- add selected preset
-  if settings.preset then
-    ---@diagnostic disable-next-line: param-type-mismatch
-    table.insert(lazy_spec, { import = "plugins." .. settings.preset })
-  end
-
-  -- add selected preset
-  if settings.lazyspec then
-    ---@diagnostic disable-next-line: param-type-mismatch
-    vim.list_extend(lazy_spec, settings.lazyspec)
-  end
-
-  ---@type LazyConfig
-  local lazy_config = {
-    spec = lazy_spec,
-    defaults = {
-      lazy = false,
-      version = false, -- always use the latest git commit
-    },
-    install = {
-      colorscheme = { settings.colorscheme, "habamax" },
-    },
-    checker = {
-      enabled = true, -- check for plugin updates periodically
-      notify = true, -- notify on update
-    },
-    performance = {
-      rtp = {
-        -- disable some rtp plugins
-        disabled_plugins = settings.disabled_plugins,
-      },
-    },
-  }
-
-  require("lazy").setup(lazy_config)
-end
-
---- Initialize configuration
-function M.init()
-  if did_init then
-    return
-  end
-  did_init = true
-
-  load_local_config()
-
-  local settings = require("c11n.settings").get()
-
-  local _ = pcall(vim.cmd.language, settings.language)
-
-  -- Load configuration for external tools
-  require("lazy.core.util").lsmod("c11n.externals", function(modname, _)
-    require(modname)
-  end)
-
-  -- Load management utilities
-  util.on_user("VeryLazy", function(_)
-    require("c11n.manage").setup()
-  end)
-
-  lazy_setup()
-end
 
 return M
